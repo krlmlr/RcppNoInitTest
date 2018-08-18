6856fbc0f1c3f4e96294914450ca0e0ce0a04ec8
Kirill Müller <krlmlr+r@mailbox.org>
2018-08-18 09:25:45 +0200

    test()

---
+++ src/test.cpp
@@ -0,0 +1,11 @@
+#include <Rcpp.h>
+
+using namespace Rcpp;
+
+//' @importFrom Rcpp compileAttributes
+//' @useDynLib RcppNoInitTest, .registration = TRUE
+// [[Rcpp::export]]
+IntegerVector test() {
+  IntegerVector ret = no_init(1);
+  return ret;
+}
